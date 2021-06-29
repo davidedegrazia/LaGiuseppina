eb6bce097d7176cac20ec47ac5df9a2f3790ae90
alessiocapriotti <s1093075@studenti.univpm.it>
2021-06-29 15:43:40 +0200

    Dipendente.py

---
+++ Progetto/dipendenti/model/Dipendente.py
@@ -0,0 +1,13 @@
+class Dipendente():
+
+    def __init__(self, nome, ore, stipendio, contratto, email, telefono, id) :
+
+        self.nome = nome
+        self.ore = ore
+        self.stipendio = stipendio
+        self.contratto = contratto
+        self.email = email
+        self.telefono = telefono
+        self.id = id
+
+
