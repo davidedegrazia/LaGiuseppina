311278294cfbc9975288e89bd2e3593b04d5b8d1
alessiocapriotti <s1093075@studenti.univpm.it>
2022-03-10 12:31:23 +0100

    model ListaProdottiConQuantità e controllore ListaProdotti

--- Progetto/listaprodotti/model/ListaProdottiSalvati.py
+++ Progetto/listaprodotti/model/ListaProdottiSalvati.py
@@ -4,7 +4,13 @@ from Progetto.magazzino.model.Magazzino import Magazzino
 class ListaProdottiSalvati():
     def __init__(self):
         super(ListaProdottiSalvati, self).__init__()
-        self.model = Magazzino()
+        self.model = Magazzino
 
     def crea_prodotto(self):
-        var = self.model.prodotto
+        self.model.prodotto
+
+    def elemento(self, prodotto):
+        self.elemento(prodotto)
+
+    def elemento_by_index(self, index):
+        self.elemento_by_index(index)
